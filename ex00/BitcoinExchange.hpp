67f706f816835ed68aa5f6c46b204f279c55e035
David Mendez Carrillo <50409052+damendez00@users.noreply.github.com>
2024-12-16 14:43:38 +0000

    start over

--- ex00/BitcoinExchange.hpp
+++ ex00/BitcoinExchange.hpp
@@ -6,36 +6,9 @@
 /*   By: damendez <damendez@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/29 14:54:47 by damendez          #+#    #+#             */
-/*   Updated: 2024/11/05 16:12:51 by damendez         ###   ########.fr       */
+/*   Updated: 2024/12/15 12:40:31 by damendez         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #pragma once
 
-#include <iostream>
-#include <iomanip>
-#include <sstream>
-#include <fstream>
-#include <map>
-#include <exception>
-#include <string>
-
-class BtcExchange
-{
-    private:
-        BtcExchange();
-        BtcExchange(const BtcExchange& rhs);
-        BtcExchange& operator=(const BtcExchange& rhs);
-        ~BtcExchange();
-
-        static bool getCSV(std::map<std::string, float> &data);
-        static bool getBTC(const std::map<std::string, float> &data, std::string infile);
-        static void parseFileLine(std::string data, float value);
-        static void parseDate(std::string date);
-        static void printCSV(const std::map<std::string, float> &data);
-        static float stringToFloat(const std::string &str);
-        static int stringToInt(const std::string &str);
-
-    public:
-        static void btc(std::string argv);
-};
--- ex00/main.cpp
+++ ex00/main.cpp
@@ -6,25 +6,21 @@
 /*   By: damendez <damendez@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/29 15:34:53 by damendez          #+#    #+#             */
-/*   Updated: 2024/11/05 15:43:21 by damendez         ###   ########.fr       */
+/*   Updated: 2024/12/15 12:49:01 by damendez         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "BitcoinExchange.hpp"
 
-int main(int argc, char **argv)
-{
-    if (argc < 2)
-    {
-        std::cerr << "Invalid number of arguments: ./btc <input.txt>";
-        return (1);
+int main(int argc, char* argv[]) {
+    
+    try {
+        // Check if valid argc
+        if (argc != 2)
+
+        // Load the Bitcoin database
+        
+        // Process input file        
     }
-    try
-    {
-        BtcExchange::btc(argv[1]);
-    }
-    catch(std::exception &e)
-    {
-        std::cout << e.what() << std::endl;   
-    }
+    catch (const exception& e)
 }
