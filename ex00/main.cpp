51ffcb9437597557e4e4f7b6781ea6b1824aaad7
David Mendez Carrillo <50409052+damendez00@users.noreply.github.com>
2024-12-27 15:17:53 +0000

    added pdf

--- ex00/main.cpp
+++ ex00/main.cpp
@@ -6,17 +6,35 @@
 /*   By: damendez <damendez@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/29 15:34:53 by damendez          #+#    #+#             */
-/*   Updated: 2024/12/17 18:50:20 by damendez         ###   ########.fr       */
+/*   Updated: 2024/12/22 20:13:55 by damendez         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "BitcoinExchange.hpp"
 
-// bool    isValidValue(const std::string& valueStr) {}
+float   myStof(const std::string& str) {
+    std::istringstream string(str);
+    float value;
+    string >> value;
+    return value;
+}
+
+std::string trim(const std::string& str) {
+    size_t start = str.find_first_not_of(" \t");
+    size_t end = str.find_last_not_of(" \t");
+    return (start == std::string::npos ? "" : str.substr(start, end - start + 1));
+}
+
+bool    isValidValue(const std::string& valueStr) {
+        float num = myStof(valueStr);
+        if (num >= 0.0f && num <= 1000.0f)
+            return true;
+        else
+            return false;
+}
 
 bool    isValidDate(const std::string& date) {
     if (date.size() != 10 || date[4] != '-' || date[7] != '-') {
-        std::cerr << "Error: bad input => " << date << std::endl;
         return false;
     }
     return true;
@@ -25,7 +43,7 @@ bool    isValidDate(const std::string& date) {
 void    processInput(const std::string& fileName, const std::map<std::string, float>& db) {
 
     // open input file
-    std::ifstream input(fileName);
+    std::ifstream input(fileName.c_str());
     if (!input.is_open())
         throw std::runtime_error("Unable to open input file.");
 
@@ -38,27 +56,38 @@ void    processInput(const std::string& fileName, const std::map<std::string, fl
         // check for invalid date/value
         std::getline(stream, date, '|');
         std::getline(stream, valueStr);
+        date = trim(date);
+        valueStr = trim(valueStr);
 
-        if (!isValidDate(date) || !isValidValue(valueStr))
+        if (!isValidDate(date) || !isValidValue(valueStr)) {
+            std::cerr << "Error: bad input in input file => " << date << " | " << valueStr << std::endl;
             continue;
+        }
 
-        float value = std::stof(valueStr);
+        std::istringstream valueStream(valueStr);
+        float value;
+        valueStream >> value;
         
         // find closest(greater) date
-        auto it = db.lower_bound(date);
-        // TO-DO
+        std::map<std::string, float>::const_iterator it = db.lower_bound(date);
+        if (it == db.begin())
+            it++;
+        else if (it == db.end()) {
+            std::cerr << "Error: no valid date found for input date " << date << std::endl;
+            continue;
+        }
         
         // calculate exchange rate and give expected output
+        float exchangeRate = it->second;
+        std::cout << date << " => " << value << " = " << value * exchangeRate << std::endl;
     }
-
-
 }
 
-void    loadDb(const std::string& fileName, const std::map<std::string, float>& db) {
+void    loadDb(const std::string& fileName, std::map<std::string, float>& db) {
 
     // open input file
-    std::ifstream inputDb(fileName);
-    if (!inputDb.is_open())
+    std::ifstream inputDb(fileName.c_str());
+    if (!inputDb.is_open() || fileName != "data.csv")
         throw std::runtime_error("Unable to open database.");
 
     // parse and trim each line
@@ -72,25 +101,24 @@ void    loadDb(const std::string& fileName, const std::map<std::string, float>& 
 
         // if date and value are valid add to db map
         date = trim(date);
+        std::cout << "data.csv date = " << date;
         valueStr = trim(valueStr);
+        std::cout << " | value = " << valueStr << std::endl;
 
-        if (isValidDate(date) && isValidValue(valueStr))
-            db[date] = std::stof(valueStr);
+        if (isValidDate(date) && isValidValue(valueStr)) {
+            float value = myStof(valueStr);
+            db[date] = value;
+        }   
     }
 }
 
 int main(int argc, char *argv[]) {
     
     try {
-        // Check if valid argc
         if (argc != 2)
             throw std::logic_error("Usage: ./btc <input file> ...");
-        
-        // Load the Bitcoin database
         std::map<std::string, float> db;
         loadDb("data.csv", db);
-        
-        // Process input file
         processInput(argv[1], db);
     }
     catch (const std::exception& e) {
